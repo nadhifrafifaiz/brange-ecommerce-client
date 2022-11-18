6be62a6502b345c1996adb9079944cced60041a4
Nadhif Rafifaiz K <nadhifrafifaiz@gmail.com>
2022-11-18 23:34:32 +0700

    chore: implement redux

--- src/pages/BaseComponents.js
+++ src/pages/BaseComponents.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import BaseTextInput from "../components/atom/BaseTextInput";
 
 const BaseComponent = () => {
+  const authGlobal = useSelector((state) => state.auth);
+
   return (
     <div className="m-4">
+      <p>{authGlobal.username}</p>
       <BaseTextInput label="Email" placeholder="Email goes here" />
     </div>
   );
---
+++ src/redux/reducers/authReducer.js
@@ -0,0 +1,16 @@
+const initialState = {
+  userId: "",
+  username: "tes",
+  email: "",
+  roleId: "",
+  isLogin: false,
+};
+
+const auth = (state = initialState, { type }) => {
+  switch (type) {
+  default:
+    return state;
+  }
+};
+
+export default auth;
